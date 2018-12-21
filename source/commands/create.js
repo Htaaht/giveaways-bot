53acb7989f4c96c84636019fc618ed856a9664f8
Htaaht <45195090+Htaaht@users.noreply.github.com>
2018-12-21 17:46:30 +0200

    Update create.js

--- source/commands/create.js
+++ source/commands/create.js
@@ -35,29 +35,3 @@ module.exports = class extends Command {
         new Giveaway(this.client, message, match);
     }
 };
-
-
-
-
-
-
-
-///////////////////
-///////////////////
-
-
-
-
-
-
-
-
-
-client.on('ready', () => {
-client.user.setGame('Camp Nacho ᶜᶰ | $help','https://www.twitch.tv/kemo355');
-console.log('Logging into discord..');
-console.log(`
-By Ҝ€₥σ
-`);
-
-});
